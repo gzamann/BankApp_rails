520160dacba4e65823b33e8543eb7fb5ace02136
aesthetism <zman97619@gmail.com>
2018-10-05 18:14:00 +0530

    callbacks_working

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,28 +1,30 @@
 class Transaction < ApplicationRecord
-  validates :t_type, presence: true, length: {maximum: 1}, inclusion: { in: %w(W D)}
+  validates :t_type, presence: true, length: {maximum: 1}, inclusion: { in: %w(w d)}
   validates :amount, presence: true, numericality:{only_integer:true}, length: {maximum: 12}
+  validate  :check_withdraw
   
   belongs_to :account
-  after_save :change_balance
+  after_initialize :do_transaction
   
   private
-		
-  def change_balance
-      if self.t_type == "W"
-          if self.amount < self.account.balance
-            remaining_balance = self.account.balance - self.amount
-            unless self.account.update_attributes(balance: remaining_balance)
-              raise "Transaction Cancelled!"
-            end
-          end
+  
+  def check_withdraw
+    if self.t_type = "w"
+      if self.amount > self.account.balance
+        errors.add(:amount, "Not enough balance in account!")
+      end
+    end
+  end
+  
+  def do_transaction
+      if self.t_type == "w"
+        updated_balance = self.account.balance - self.amount
+      else
+        updated_balance = self.account.balance + self.amount
       end
 
-      if self.t_type == "D"
-        new_balance = self.account.balance + self.amount
-        unless self.account.update_attributes(balance: new_balance)
-          raise "Transaction Cancelled!"
-        end	
+      unless self.account.update_attributes(balance: updated_balance)
+        raise "Transaction Cancelled!"
       end
   end
-
 end
--- db/migrate/20181004141902_create_accounts.rb
+++ db/migrate/20181004141902_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
-      t.decimal :balance
+      t.integer :balance
       t.string :a_type
       t.integer :number
       
