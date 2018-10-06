42694fb025e48301e402bb8fa030a83759b5489a
aesthetism <zman97619@gmail.com>
2018-10-06 11:41:57 +0530

    loan_types

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,5 +1,5 @@
 class Loan < ApplicationRecord
-  validates :loan_type, presence: true, length: {maximum: 10}
+  validates :loan_type, presence: true, length: {maximum: 10}, inclusion: {in: %w(Home Personal Education Business)}
   validates :amount, presence: true,  numericality: {only_integer: true}, length: {maximum: 12}
   
   belongs_to :client
