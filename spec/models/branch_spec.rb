88b26429f516fc246dae775e8afc4b3e452da513
aesthetism <zman97619@gmail.com>
2018-10-12 09:17:54 +0530

    branch_spec+tests

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Branch, type: :model do
   context 'validate' do
-  it 'is a valid factory' do
-    branch = FactoryBot.build(:branch)
-    expect(branch).to be_valid
+    it 'is a valid factory' do
+      branch = FactoryBot.build(:branch)
+      expect(branch).to be_valid
+    end
+    it 'is invalid to have address nil' do
+      expect(FactoryBot.build(:branch, address: nil)).to be_invalid
+    end
+    it 'is invalid to have ifsc nil' do
+      expect(FactoryBot.build(:branch, ifsc: nil)).to be_invalid
+    end
+    it 'is invalid to have number nil' do
+      expect(FactoryBot.build(:branch, number: nil)).to be_invalid
+    end
+    it 'is invalid to exceed 12 char for ifsc' do
+      expect(FactoryBot.build(:branch, ifsc: '1234567891234')).to be_invalid
+    end
+    it 'is invalid take string for number' do
+      expect(FactoryBot.build(:branch, number: 'asdf12')).to be_invalid
+    end
+    it 'is invalid to exceed 10 digit for number' do
+      expect(FactoryBot.build(:branch, number: '12345678912')).to be_invalid
+    end
   end
-  it 'is invalid to have address nil' do
-    expect(FactoryBot.build(:branch, address: nil)).to be_invalid
-  end
-  it 'is invalid to exceed 12 char for ifsc' do
-    expect(FactoryBot.build(:branch, ifsc: '1234567891234')).to be_invalid
-  end
-  it 'is invalid take string for number'
-    expect(FactoryBot.build(:branch, number: 'asdf12')).to be_invalid
-  end
-end
 end
